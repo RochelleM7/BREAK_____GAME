6b71c488dc63969f7a1202b6ab1cbf0c809ef3af
RochelleM7 <444205625@qq.com>
2014-12-04 17:34:47 -0600

    Update wall.h

--- wall.h
+++ wall.h
@@ -1,3 +1,14 @@
+/*
+Authors:             Chuks Egbuchunam, Zehao Huang, Jiaqi Ma
+                     Brandon Scheitlin, Trent Matthews
+Assignment Title:    Breakout
+Assignment Details:  Make a game that breaks blocks
+                     with a ball and paddle.
+Due Date:            12/05/2014
+Date Created:        11/11/2014
+Date Last Modified:  12/04/2014
+*/
+
 #ifndef WALL_H_INCLUDED
 #define WALL_H_INCLUDED
 
@@ -6,15 +17,25 @@
 class sideBoundariesReflections
 {
   public:
+  
+  // description: calculate the reflection of the top boundary
+  // return:      double
+  // pre:         object exists
+  // post:        return the reflection of the top boundary
   double topBoundaryReflection(ball b);
+  
+  // description: calculate the reflection of the left boundary
+  // return:      double
+  // pre:         object exists
+  // post:        return the reflection of the left boundary
   double leftBoundaryReflection(ball b);
+  
+  // description: calculate the reflection of the right boundary
+  // return:      double
+  // pre:         object exists
+  // post:        return the reflection of the right boundary
   double rightBoundaryReflection(ball b);
-
-
-
-
-
-
+  
 };
 
 #endif // WALL_H_INCLUDED
