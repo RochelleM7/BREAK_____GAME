007c1e0010950a7cabd3ff79d85dd6eab63688cd
RochelleM7 <444205625@qq.com>
2014-12-04 17:10:02 -0600

    Update wall.h [with documentations]

--- wall.h
+++ wall.h
@@ -6,13 +6,24 @@
 class sideBoundariesReflections
 {
   public:
+  
+  // description: calculate the reflection of the top boundary
+  // return:      double
+  // pre:         object exists
+  // post:        return the reflection of the top boundary
   double topBoundaryReflection(ball b);
+  
+  // description: calculate the reflection of the left boundary
+  // return:      double
+  // pre:         object exists
+  // post:        return the reflection of the left boundary
   double leftBoundaryReflection(ball b);
+  
+  // description: calculate the reflection of the right boundary
+  // return:      double
+  // pre:         object exists
+  // post:        return the reflection of the right boundary
   double rightBoundaryReflection(ball b);
-
-
-
-
 
 
 };
