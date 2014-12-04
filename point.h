55e89daedccc211542d5b7c718e7382daa4b5455
RochelleM7 <444205625@qq.com>
2014-12-04 17:00:55 -0600

    Update point.h

--- point.h
+++ point.h
@@ -9,11 +9,40 @@ class Point
 {
 
     public:
+        // description: set default value of point x and y
+        // return:      no return type                                        
+        // pre:         no                             
+        // post:        object exists
         Point (double xVal = 3, double yVal = 5);
+        
+        // description: get value of point x
+        // return:      double                                        
+        // pre:         object exists                           
+        // post:        return the value of point x
         double getX () const;
+        
+        // description: get value of point y
+        // return:      double                                        
+        // pre:         object exists                           
+        // post:        return the value of point y
         double getY () const;
+        
+        // description: set value of point x
+        // return:      no return type                                        
+        // pre:         no                             
+        // post:        object exists
         void setX (double);
+        
+        // description: set value of point y
+        // return:      no return type                                        
+        // pre:         no                             
+        // post:        object exists
         void setY (double);
+        
+        // description: calculate the distance
+        // return:      double                                        
+        // pre:         object exists                          
+        // post:        the value of distance
         double distance (Point p);
 
     private:
