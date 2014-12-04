6f745cf9b72459854c86f0994eedffe656729c44
RochelleM7 <444205625@qq.com>
2014-12-04 17:51:46 -0600

    Update paddle.h

--- paddle.h
+++ paddle.h
@@ -1,3 +1,14 @@
+/*
+Authors:             Chuks Egbuchunam, Zehao Huang, Jiaqi Ma
+                     Brandon Scheitlin, Trent Matthews
+Assignment Title:    Breakout
+Assignment Details:  Make a game that breaks blocks
+                     with a ball and paddle.
+Due Date:            12/05/2014
+Date Created:        11/11/2014
+Date Last Modified:  12/04/2014
+*/
+
 #ifndef PADDLE_H_INCLUDED
 #define PADDLE_H_INCLUDED
 
@@ -6,16 +17,79 @@
 class paddleType
 {
     public:
+       
+        // description: set default value of width, colors, rows and colume
+        // return:      no return type
+        // pre:         no
+        // post:        object exists 
         paddleType (int w = 30, ink c = red, int r = 97, int col = 240);
+        
+        // description: draw the paddle
+        // return:      no return type
+        // pre:         no
+        // post:        object exists 
         void draw (Plotter&);
+        
+        // description: erase the color
+        // return:      no return type
+        // pre:         no
+        // post:        object exists 
         void erase (Plotter&);
+        
+        // description: move the paddle
+        // return:      no return type
+        // pre:         no
+        // post:        object exists 
         void move (int);
+        
+        // description: check if the paddle is hit by the ball
+        // return:      bool
+        // pre:         object exists 
+        // post:        return hit
         bool hitByBall(ball);
+        
+        // description: check if the paddle hit the wall
+        // return:      bool
+        // pre:         object exists
+        // post:        HitWall
         bool paddleHitWall(paddleType, int);
+        
+        // description: take the point 
+        // return:      int
+        // pre:         object exists
+        // post:        return the location of the paddle
         int getLoc();
+        
+        // description: calculate the direction of the ball
+        //              when the ball hit the outer of paddle
+        // return:      double
+        // pre:         object exists 
+        // post:        return direction
         double ballHitOuterPaddle(ball b);
+        
+        // description: calculate the direction of the ball
+        //              when the ball hit the inner of paddle
+        // return:      double
+        // pre:         object exists 
+        // post:        return direction
         double ballHitInnerPaddle(ball b);
+        
+        // description: calculate the direction of the ball
+        //              when the ball hit the middle of paddle
+        // return:      double
+        // pre:         object exists 
+        // post:        return direction
         double ballHitMiddlePaddle(ball b);
+        
+        // description: make sure that the ball in stuck to the 
+        //              paddle and the user can move the paddle
+        //              to anywhere he likes before the start of
+        //              the game
+        // return:      no return type
+        // pre:         no
+        // post:        object exists
+        void   ballStuckToPaddle(ball, Plotter&);
+        
     private:
         int width;
         Point loc, oldLoc;
